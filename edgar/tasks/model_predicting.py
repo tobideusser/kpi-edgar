b6cd4d0d9d9d8c17798154e73767a169d6c54f14
Tobi Deusser <tobias.deusser@iais.fraunhofer.de>
2022-08-29 17:00:43 +0200

    added python black to model_predicting.py, also a small bug fix in this file

--- edgar/tasks/model_predicting.py
+++ edgar/tasks/model_predicting.py
@@ -9,28 +9,28 @@ from tqdm import tqdm
 from transformers import PreTrainedTokenizerFast
 
 from edgar.data_classes import Corpus, Labels, Entity, Relation
-from edgar.trainer import Evaluator
-from edgar.trainer.utils import set_device, get_device, set_seed_number, set_seeds, detach_batch
 from edgar.data_loading import KPIRelationDataset, BatchCollator
 from edgar.html_creation import create_html
 from edgar.models import JointNERAndREModel
+from edgar.trainer import Evaluator
+from edgar.trainer.utils import set_device, get_device, set_seed_number, set_seeds, detach_batch
 
 logger = logging.getLogger(__name__)
 
 
 class ModelPredicting(Task):
-    publishes = ['corpus_predicted', 'metrics']
+    publishes = ["corpus_predicted", "metrics"]
 
     def __init__(
-            self,
-            split_types: Optional[Union[str, List]] = None,
-            model_params: Optional[Dict] = None,
-            evaluator_params: Optional[Dict] = None,
-            batch_size: Optional[int] = None,
-            seed: Optional[int] = None,
-            evaluation: bool = True,
-            html_creation: bool = True,
-            train_mode: bool = True
+        self,
+        split_types: Optional[Union[str, List]] = None,
+        model_params: Optional[Dict] = None,
+        evaluator_params: Optional[Dict] = None,
+        batch_size: Optional[int] = None,
+        seed: Optional[int] = None,
+        evaluation: bool = True,
+        html_creation: bool = True,
+        train_mode: bool = True,
     ):
         super().__init__()
 
@@ -45,16 +45,13 @@ class ModelPredicting(Task):
         self.train_mode = train_mode
 
     def _predict_corpus(
-            self,
-            model: JointNERAndREModel,
-            dataloader: DataLoader,
-            evaluator: Optional[Evaluator] = None
+        self, model: JointNERAndREModel, dataloader: DataLoader, evaluator: Optional[Evaluator] = None
     ) -> Tuple[Dict, Dict]:
 
-        logger.debug('Predict corpus...')
+        logger.debug("Predict corpus...")
         corpus_predictions = {}
         for batch in tqdm(dataloader, desc="Predict batch"):
-            batch_size = batch['token_ids'].shape[0]
+            batch_size = batch["token_ids"].shape[0]
 
             batch_output = model.predict(batch)
 
@@ -66,11 +63,11 @@ class ModelPredicting(Task):
                 evaluator.increment_metrics(batch_output=batch_output)
 
             for i in range(batch_size):
-                corpus_predictions[batch['unique_id'][i]] = {
-                    'entities_pred': batch_output['entities_pred'][i],
-                    'relations_pred': batch_output['relations_pred'][i],
-                    'entities_score': batch_output['ner_score'][i],
-                    'relations_score': batch_output['re_scores'][i],
+                corpus_predictions[batch["unique_id"][i]] = {
+                    "entities_pred": batch_output["entities_pred"][i],
+                    "relations_pred": batch_output["relations_pred"][i],
+                    "entities_score": batch_output["ner_score"][i],
+                    "relations_score": batch_output["re_scores"][i],
                 }
         metrics = None
         if self.evaluation:
@@ -80,31 +77,29 @@ class ModelPredicting(Task):
 
     @staticmethod
     def _write_predictions_to_corpus(corpus: Corpus, corpus_predictions: Dict) -> Corpus:
-        logger.debug('Write predictions in corpus...')
-        for sentence in tqdm(corpus.sentences, desc='Write predictions'):
+        logger.debug("Write predictions in corpus...")
+        for sentence in tqdm(corpus.sentences, desc="Write predictions"):
             sentence_preds = corpus_predictions.get(sentence.unique_id)
             if sentence_preds:
                 # convert predicted entities in correct format
                 sentence.entities_pred = [
-                    Entity.from_dict({'start': start,
-                                      'end': end,
-                                      'type_': type_,
-                                      'score': score})
-                    for score, ((start, end), type_) in zip(sentence_preds['entities_score'],
-                                                            sentence_preds['entities_pred'].items())
+                    Entity.from_dict({"start": start, "end": end, "type_": type_, "score": score})
+                    for score, ((start, end), type_) in zip(
+                        sentence_preds["entities_score"], sentence_preds["entities_pred"].items()
+                    )
                 ]
 
                 # convert predicted relations in correct format
                 entity_spans = [ent.span for ent in sentence.entities_pred]
-                for score, rel_pred in zip(sentence_preds['relations_score'], sentence_preds['relations_pred']):
-                    head_span = rel_pred.pop('head')
-                    tail_span = rel_pred.pop('tail')
-                    rel_pred['head_idx'] = entity_spans.index(head_span)
-                    rel_pred['tail_idx'] = entity_spans.index(tail_span)
-                    del rel_pred['head_type']
-                    del rel_pred['tail_type']
-                    rel_pred['score'] = score
-                sentence.relations_pred = [Relation.from_dict(rel) for rel in sentence_preds['relations_pred']]
+                for score, rel_pred in zip(sentence_preds["relations_score"], sentence_preds["relations_pred"]):
+                    head_span = rel_pred.pop("head")
+                    tail_span = rel_pred.pop("tail")
+                    rel_pred["head_idx"] = entity_spans.index(head_span)
+                    rel_pred["tail_idx"] = entity_spans.index(tail_span)
+                    del rel_pred["head_type"]
+                    del rel_pred["tail_type"]
+                    rel_pred["score"] = score
+                sentence.relations_pred = [Relation.from_dict(rel) for rel in sentence_preds["relations_pred"]]
 
         return corpus
 
@@ -125,25 +120,39 @@ class ModelPredicting(Task):
                     rel.get_entities(sent.entities_pred)
         return corpus_predicted
 
-    def run(self,
-            best_model: Dict,
-            corpus_tokenized: Union[Dict, Corpus],
-            labels: Dict,
-            sub_word_tokenizer: PreTrainedTokenizerFast):
+    def run(
+        self,
+        best_model: Dict,
+        corpus_tokenized: Union[Dict, Corpus],
+        labels: Dict,
+        sub_word_tokenizer: PreTrainedTokenizerFast,
+    ):
 
         try:
             self.seed = self.seed if self.seed is not None else self.unique_config["ModelTraining"]["seed"]
-            self.model_params = self.model_params if self.model_params is not None \
+            self.model_params = (
+                self.model_params
+                if self.model_params is not None
                 else self.unique_config["ModelTraining"]["model_params"]
-            self.evaluator_params = self.evaluator_params if self.evaluator_params is not None \
+            )
+            self.evaluator_params = (
+                self.evaluator_params
+                if self.evaluator_params is not None
                 else self.unique_config["ModelTraining"]["evaluator_params"]
+            )
 
         except KeyError:
             self.seed = self.seed if self.seed is not None else self.unique_config["ModelSelfTraining"]["seed"]
-            self.model_params = self.model_params if self.model_params is not None \
+            self.model_params = (
+                self.model_params
+                if self.model_params is not None
                 else self.unique_config["ModelSelfTraining"]["model_params"]
-            self.evaluator_params = self.evaluator_params if self.evaluator_params is not None \
+            )
+            self.evaluator_params = (
+                self.evaluator_params
+                if self.evaluator_params is not None
                 else self.unique_config["ModelSelfTraining"]["evaluator_params"]
+            )
 
         # because type_ and path_pretrained_model not required in model predicting task
         _ = self.model_params.pop("type_")
@@ -154,45 +163,50 @@ class ModelPredicting(Task):
         set_seeds()
 
         if isinstance(corpus_tokenized, Dict):
-            logger.info('Converting corpus_tokenized dict to Corpus object.')
+            logger.info("Converting corpus_tokenized dict to Corpus object.")
             corpus = Corpus.from_dict(corpus_tokenized)
         else:
             corpus = corpus_tokenized
 
         labels = Labels.from_dict(labels)
-        sub_word_tokenizer.type_ = self.unique_config['SubWordTokenization']['tokenizer_name']
+        sub_word_tokenizer.type_ = self.unique_config["SubWordTokenization"]["tokenizer_name"]
 
         # load model architecture / untrained model
-        logger.debug('Instantiate torch model and load state dict.')
-        model = JointNERAndREModel.from_config(tokenizer=sub_word_tokenizer,
-                                               labels=labels,
-                                               **self.model_params).to(get_device())
+        logger.debug("Instantiate torch model and load state dict.")
+        model = JointNERAndREModel.from_config(tokenizer=sub_word_tokenizer, labels=labels, **self.model_params).to(
+            get_device()
+        )
         # load model state dict into model and set model in validation mode
         model.load_state_dict(best_model)
         model.eval()
 
-        logger.debug('Instantiate torch dataset.')
+        logger.debug("Instantiate torch dataset.")
+        try:
+            max_position_embeddings = model.encoder.encoder.config.max_position_embeddings
+        except AttributeError:
+            max_position_embeddings = float("inf")
         if self.split_types:
-            sentences = [sentence for sentence in corpus.sentences
-                         if sentence.split_type in self.split_types
-                         and len(sentence.token_ids) <= model.encoder.encoder.config.max_position_embeddings]
+            sentences = [
+                sentence
+                for sentence in corpus.sentences
+                if sentence.split_type in self.split_types and len(sentence.token_ids) <= max_position_embeddings
+            ]
         else:
-            sentences = [sentence for sentence in corpus.sentences
-                         if len(sentence.token_ids) <= model.encoder.encoder.config.max_position_embeddings]
+            sentences = [
+                sentence for sentence in corpus.sentences if len(sentence.token_ids) <= max_position_embeddings
+            ]
         # TODO change back
         dataset = KPIRelationDataset(sentences=sentences)
         batch_collator = BatchCollator(pad_token_id=sub_word_tokenizer.pad_token_id)
 
-        logger.debug('Instantiate torch dataloader.')
-        dataloader = DataLoader(dataset=dataset,
-                                collate_fn=batch_collator,
-                                shuffle=False,
-                                batch_size=self.batch_size,
-                                drop_last=False)
+        logger.debug("Instantiate torch dataloader.")
+        dataloader = DataLoader(
+            dataset=dataset, collate_fn=batch_collator, shuffle=False, batch_size=self.batch_size, drop_last=False
+        )
 
         evaluator = None
         if self.evaluation:
-            logger.debug('Instantiate evaluator.')
+            logger.debug("Instantiate evaluator.")
             evaluator = Evaluator.from_config(**self.evaluator_params)
 
         # predict corpus
@@ -205,20 +219,20 @@ class ModelPredicting(Task):
         corpus = self._register_entity_values_and_relation_entities(corpus)
 
         if self.html_creation:
-            logger.info('Create html evaluation.')
+            logger.info("Create html evaluation.")
             current_run_dir = self.get_store_context()
-            html_report_dir = os.path.join(current_run_dir, 'html')
+            html_report_dir = os.path.join(current_run_dir, "html")
             os.makedirs(html_report_dir, exist_ok=True)
 
             if self.split_types:
                 sentences = [sentence for sentence in corpus.sentences if sentence.split_type in self.split_types]
             else:
                 sentences = [sentence for sentence in corpus.sentences]
-            create_html(sentences, out_dir=os.path.join(current_run_dir, 'html'))
+            create_html(sentences, out_dir=os.path.join(current_run_dir, "html"))
 
         if self.train_mode:
-            self.save(corpus.to_dict(), 'corpus_predicted', type_='pickle')
+            self.save(corpus.to_dict(), "corpus_predicted", type_="pickle")
             if metrics:
-                self.save(metrics, 'metrics', type_='json')
+                self.save(metrics, "metrics", type_="json")
         else:
             return corpus
